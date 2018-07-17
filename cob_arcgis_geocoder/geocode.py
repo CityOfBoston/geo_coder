1810777e826fe08f9300d65df0f2716d2a82a0dc
Jonathan Porter <JPHaus@users.noreply.github.com>
2018-07-17 14:10:56 -0400

    Added method for archiving addresses without a SAM ID

--- cob_arcgis_geocoder/geocode.py
+++ cob_arcgis_geocoder/geocode.py
@@ -2,6 +2,13 @@ import pandas as pd
 import urllib.parse
 import json
 from pandas.io.json import json_normalize
+import psycopg2
+import subprocess
+import os
+import codecs
+from collections import OrderedDict
+import sys
+from datetime import datetime
 
 class CobArcGISGeocoder(object):
     # TODO: write failed addresses to a table
@@ -14,12 +21,12 @@ class CobArcGISGeocoder(object):
     def geocode_df(self):
         
         # add columns for geocoded address information
-        df = pd.concat([self.df,pd.DataFrame(columns=list(["matched_address", "matched_address_score", "SAM_ID", "location_x", "location_y", "flag", "reverse_geocode_address"]))])
+        df = pd.concat([self.df,pd.DataFrame(columns=["matched_address", "matched_address_score", "SAM_ID", "location_x", "location_y", "flag", "locator_name"])])
         
         # Locators that return addresses with SAM IDs
         SAM_Locators = ["SAM_Sub_Unit_A", "SAM_Alternate"]
 
-        # interate through each row and geocode the address
+        # Iterate through each row and geocode the address
         for index, row in df.iterrows():
             
             if row[self.address_field] is None: 
@@ -40,13 +47,17 @@ class CobArcGISGeocoder(object):
                     df.at[index, "location_x"] = matched_address_df[["location.x"]][0]
                     df.at[index, "location_y"] = matched_address_df[["location.y"]][0]
                     df.at[index, "flag"] = matched_address_df[["flag"]][0]
+                    df.at[index, "locator_name"] = matched_address_df[["attributes.Loc_name"]][0]
+                elif matched_address_df[["flag"]][0] == "Able to geocode to a non-SAM address.":
+                    self._archive_non_sam_address(df.at[index], "Non-SAM address")
                 else:
                     # if unable to find an address to geocode to, flag the row in the dataframe
-                    # TODO: write this row to a table in postgres so we can log failures
                     df.at[index, "flag"] = "Unable to geocode to any address."
                     # Set lat/long to 0 if unable to geocode
                     df.at[index, "location_x"] = 0.00
                     df.at[index, "location_y"] = 0.00      
+                    # TODO: write this row to a table in postgres so we can log failures
+                    self._archive_non_sam_address(df.at[index], "Unable to geocode")
 
         # return the updated dataframe when the rows have been iterated through
         return df
@@ -95,7 +106,7 @@ class CobArcGISGeocoder(object):
             none: If there were no candidates returned return None.
         """
 
-        if len(candidates["candidates"]) >= 1:
+        if len(candidates["candidates"]) > 0:
 
             # if there is at least 1 candidate, put the results into a dataframe
             addresses_df = json_normalize(candidates["candidates"])
@@ -106,15 +117,16 @@ class CobArcGISGeocoder(object):
             if len(addresses_df_SAM.index) == 0:
                 print("there were {} SAM address candidates".format(len(addresses_df_SAM.index)))
 
-                # if there are no SAM addresses, try to reverse geocode the highest scored candidate
-                matched_address_df = self._reverse_geocode(addresses_df, locators)
+                # if there are no SAM addresses, return the highest scored locator
+                matched_address_df = addresses_df[["address", "score", "attributes.Ref_ID", "location.x", "location.y, attributes.Loc_Name"]].sort_values(by="score", ascending=False).iloc[0]
+                matched_address_df["flag"] = "Able to geocode to a non-SAM address."
                 return matched_address_df
             
             else:
                 print("there are {} SAM addresses".format(len(addresses_df_SAM.index)))
 
                 # sort values by score and pick the highest one to return - **Ref_ID is the SAM ID**
-                matched_address_df = addresses_df_SAM[["address", "score", "attributes.Ref_ID", "location.x", "location.y"]].sort_values(by="score", ascending=False).iloc[0]
+                matched_address_df = addresses_df_SAM[["address", "score", "attributes.Ref_ID", "location.x", "location.y, attributes.Loc_Name"]].sort_values(by="score", ascending=False).iloc[0]
                 
                 # add flag to dataframe and return it
                 matched_address_df["flag"] = "Able to geocode to a SAM address."
@@ -123,3 +135,43 @@ class CobArcGISGeocoder(object):
         else:
             # if there were no candidates returned, return None so the row in the dataframe can be properly flagged
             return None
+    
+    @classmethod
+    def _archive_non_sam_address(self, df, source):
+        """Uploads to a postgres table to keep track of addresses that need to be assigned a SAM ID."""
+        try:
+            env_var_dict = dict()
+            env_var_dict['upload_hostname'] = os.environ.get("POSTGRES_IP")
+            env_var_dict['upload_database_name'] = os.environ.get("POSTGRES_PROD_DB")
+            env_var_dict['upload_database_user'] = os.environ.get("POSTGRES_PROD_USER")
+            env_var_dict['upload_database_pass'] = os.environ.get("POSTGRES_PROD_PASS")
+        
+        # If environment variables don't exist, continue to run without archiving data
+        except:
+            return
+
+        config_params = dict()
+        config_params['upload_table_name'] = "internal_data.failed_geocoded_addresses"
+        config_params['upload_database_port'] = "6666"
+
+
+        conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}' port={}".format(env_var_dict['upload_database_name'], env_var_dict['upload_database_user'], env_var_dict['upload_hostname'], env_var_dict['upload_database_pass'], int(config_params['upload_database_port']) ))
+        cur = conn.cursor()
+
+        # Write the latest addresses to the table
+        try:
+            insert_query = "INSERT INTO {} (address_submitted, source, time_stamp) VALUES ('{}', '{}', '{}')".format(config_params['upload_table_name'], df['address_field'], source, datetime.now())
+            
+            print(insert_query)
+
+            cur.execute(insert_query)
+            conn.commit()
+
+            print("Response from inserting into the {} table: {}".format(config_params['upload_table_name'], cur.statusmessage))
+            
+            if 'insert' not in cur.statusmessage.lower():
+                print("Incorrect status message from insert operation. Exiting. An error occured.")
+                sys.exit(1)
+
+        except Exception as e:
+            print("ERROR: Issue inserting data into the database. Exiting. Error: {}".format(e))
