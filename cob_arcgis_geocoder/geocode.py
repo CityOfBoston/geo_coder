8377e9c065600e74344f3386fb198235fd56f40b
Jonathan Porter <JPHaus@users.noreply.github.com>
2018-07-17 16:40:47 -0400

    debugging

--- cob_arcgis_geocoder/geocode.py
+++ cob_arcgis_geocoder/geocode.py
@@ -123,8 +123,6 @@ class CobArcGISGeocoder(object):
             else:
                 print("there are {} SAM addresses".format(len(addresses_df_SAM.index)))
                 # sort values by score and pick the highest one to return - **Ref_ID is the SAM ID**
-                columns = ["address", "score", "attributes.Ref_ID", "location.x", "location.y, attributes.Loc_name"]
-                addresses_df_SAM = addresses_df_SAM.reindex(columns=columns)
                 matched_address_df = addresses_df_SAM[["address", "score", "attributes.Ref_ID", "location.x", "location.y, attributes.Loc_name"]].sort_values(by="score", ascending=False).iloc[0]
                 
                 # add flag to dataframe and return it
