2900cdc3ed09eb6b66645380fe9d02ac47050eb5
Jonathan Porter <JPHaus@users.noreply.github.com>
2018-07-17 15:56:04 -0400

    debugging

--- cob_arcgis_geocoder/geocode.py
+++ cob_arcgis_geocoder/geocode.py
@@ -124,7 +124,7 @@ class CobArcGISGeocoder(object):
             
             else:
                 print("there are {} SAM addresses".format(len(addresses_df_SAM.index)))
-
+                print(addresses_df_SAM)
                 # sort values by score and pick the highest one to return - **Ref_ID is the SAM ID**
                 matched_address_df = addresses_df_SAM[["address", "score", "attributes.Ref_ID", "location.x", "location.y, attributes.Loc_Name"]].sort_values(by="score", ascending=False).iloc[0]
                 
