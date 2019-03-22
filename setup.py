f047e3f4bf00f3b87d7adc48f423e6eefdae9696
bmackattack <brian.mcmahon@boston.gov>
2019-03-22 17:01:02 -0400

    initial commit of reverse_geocoder

--- setup.py
+++ setup.py
@@ -13,3 +13,12 @@ setup(name='cob_arcgis_geocoder',
       author_email='many',
       packages=['cob_arcgis_geocoder'],
 zip_safe=False)
+
+setup(name='cob_arcgis_reverse_geocoder',
+      version='0.0.1',
+      description='Python reverse geocoder to be used in ETL pipelines',
+      url='later',
+      author='Brian McMahon',
+      author_email='many',
+      packages=['cob_arcgis_reverse_geocoder'],
+zip_safe=False)
